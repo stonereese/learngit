2d07070336580741ab405f26289d50cef243b37c
LC <xuzs0185@gmail.com>
2019-08-30 16:44:52 +0800

    修改test.py的注释文件,测试合并是否冲突

--- test.py
+++ test.py
@@ -2,6 +2,6 @@
 import pandas_profiling
 import pandas as pd
 data = pd.read_excel(r"C:\Users\S\Desktop\录音文件\截止到0820_总表去除重复后.xlsx")
-mypath = "c:/users/s/desktop"
-# mypath=input("输入一个路径:\n")
+# mypath = "c:/users/s/desktop"
+mypath=input("输入一个路径:\n")
 print(data.head())
